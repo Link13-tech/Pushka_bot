66132959be62422d59f8712cfd6456e31fbdd46f
Link13-tech <151656048+Link13-tech@users.noreply.github.com>
2024-12-03 19:37:04 +0300

    fix com

--- levels/training_levels.py
+++ levels/training_levels.py
@@ -13,10 +13,10 @@ def apply_difficulty_level(text: str, difficulty_level: int) -> str:
     """
 
     def apply_replacement(word: str, level: int) -> str:
-        # Используем регулярное выражение для разделения слова на буквы и знаки (включая дефисы)
-        match = re.match(r"([а-яА-ЯёЁa-zA-Z\-]+)(\W*)", word)  # Добавлен дефис в регулярное выражение
+        # Используем регулярное выражение для разделения слова на буквы и знаки
+        match = re.match(r"([а-яА-ЯёЁa-zA-Z\-]+)(\W*)", word)
         if not match:
-            return word  # Если это не слово, возвращаем его без изменений
+            return word
 
         core, punctuation = match.groups()
         word_len = len(core)
