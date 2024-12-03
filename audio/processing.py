95133281e62d9bf8a6b20d73df2ef18db361df02
Link13-tech <151656048+Link13-tech@users.noreply.github.com>
2024-12-03 19:54:49 +0300

    added yandex recogn

--- audio/processing.py
+++ audio/processing.py
@@ -1,10 +1,21 @@
+import os
 import re
-
+from dotenv import load_dotenv
 import soundfile as sf
-import speech_recognition as sr
 from pydub import AudioSegment
-import noisereduce as nr
 import librosa
+import noisereduce as nr
+from speechkit import model_repository, configure_credentials, creds
+from speechkit.stt import AudioProcessingType
+
+load_dotenv()
+
+# Конфигурация API-ключа Yandex SpeechKit
+configure_credentials(
+    yandex_credentials=creds.YandexCredentials(
+        api_key=os.getenv("YANDEX_API_KEY")
+    )
+)
 
 
 # Конвертация аудио из OGG в WAV
@@ -15,35 +26,48 @@ def convert_ogg_to_wav(input_path: str, output_path: str):
 
 # Фильтрация шума
 def reduce_noise(input_path: str, output_path: str):
-    y, sr_rate = librosa.load(input_path, sr=None)  # Загрузка аудио
-    reduced_noise = nr.reduce_noise(y=y, sr=sr_rate)  # Уменьшение шума
-    sf.write(output_path, reduced_noise, sr_rate)  # Запись очищенного аудио в файл
+    y, sr_rate = librosa.load(input_path, sr=None)
+    reduced_noise = nr.reduce_noise(y=y, sr=sr_rate)
+    sf.write(output_path, reduced_noise, sr_rate)
+
+
+# Изменение частоты дискретизации
+def change_sample_rate(input_path: str, output_path: str, target_sr: int):
+    y, sr = librosa.load(input_path, sr=None)
+    y_resampled = librosa.resample(y, sr, target_sr)
+    sf.write(output_path, y_resampled, target_sr)
+
+
+# Подготовка аудио для распознавания (снижение шума и изменение частоты дискретизации)
+def prepare_audio(input_path: str, output_path: str, target_sr: int):
+    reduce_noise(input_path, output_path)
+    change_sample_rate(output_path, output_path, target_sr)
 
 
 # Распознавание текста из аудио
 def recognize_speech_from_audio(file_path: str) -> str:
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(file_path) as source:
-        audio = recognizer.record(source)
+    model = model_repository.recognition_model()
+
+    model.model = 'general'
+    model.language = 'ru-RU'
+    model.audio_processing_type = AudioProcessingType.Full
+
     try:
-        return recognizer.recognize_google(audio, language="ru-RU")
-    except sr.UnknownValueError:
-        return "Не удалось распознать текст."
-    except sr.RequestError as e:
-        return f"Ошибка сервиса распознавания: {e}"
+        result = model.transcribe_file(file_path)
+        transcript = result[0].raw_text
+        return transcript.strip()
+    except Exception as e:
+        return f"Ошибка при распознавании речи: {e}"
 
 
-# Функция для очистки текста от знаков препинания и приведения к нижнему регистру
+# Очистка текста
 def clean_text(text: str) -> str:
-    # Удаление всех знаков препинания
     cleaned_text = re.sub(r'[^\w\s]', '', text)
-    # Приведение текста к нижнему регистру
     return cleaned_text.lower()
 
 
-# Функция для объединения всех строк в одну
+# Объединение всех строк текста
 def merge_lines(text: str) -> str:
-    # Заменяем символы новой строки на пробелы
     return ' '.join(text.splitlines())
 
 
