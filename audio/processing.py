c85e23581a640ed1d58e0d672b1176a3effe7eae
Link13-tech <151656048+Link13-tech@users.noreply.github.com>
2024-12-06 00:18:26 +0300

    fix remove_overlap_duplicates processing.py

--- audio/processing.py
+++ audio/processing.py
@@ -21,6 +21,30 @@ def reduce_noise(input_path: str, output_path: str):
     sf.write(output_path, reduced_noise, sr_rate)
 
 
+# Разбиение аудио на 40-секундные фрагменты с перекрытием и удалением дубликатов на стыке фрагментов
+def split_audio(input_path: str, segment_length_ms: int = 40000, overlap_ms: int = 1500, min_segment_length_ms: int = 2000):
+    audio = AudioSegment.from_wav(input_path)
+    duration_ms = len(audio)
+
+    if duration_ms > segment_length_ms:
+        print(f"Длительность аудио больше 40 секунд ({duration_ms / 1000} секунд). Разбиваем на фрагменты.")
+        segments = []
+        for i in range(0, duration_ms, segment_length_ms - overlap_ms):
+            segment = audio[i:i + segment_length_ms]
+
+            if len(segment) < min_segment_length_ms:
+                continue
+
+            segment_path = f"audio/segment_{i // (segment_length_ms - overlap_ms)}.wav"
+            segment.export(segment_path, format="wav")
+            segments.append(segment_path)
+
+        return segments
+    else:
+        print(f"Длительность аудио меньше или равна 40 секундам ({duration_ms / 1000} секунд).")
+        return [input_path]
+
+
 # Распознавание текста из аудио
 def recognize_speech_from_audio(file_path: str) -> str:
     recognizer = sr.Recognizer()
@@ -28,20 +52,57 @@ def recognize_speech_from_audio(file_path: str) -> str:
     segments = split_audio(file_path)
 
     recognized_text = ""
+    previous_segment_text = []
 
     # Обрабатываем каждый сегмент
-    for segment_path in segments:
+    for i, segment_path in enumerate(segments):
         with sr.AudioFile(segment_path) as source:
             audio = recognizer.record(source)
+
         try:
             segment_text = recognizer.recognize_google(audio, language="ru-RU")
-            recognized_text += " " + segment_text
         except sr.UnknownValueError:
-            recognized_text += " [Не удалось распознать текст] "
+            segment_text = " [Не удалось распознать текст] "
         except sr.RequestError as e:
-            recognized_text += f" [Ошибка сервиса распознавания: {e}] "
+            segment_text = f" [Ошибка сервиса распознавания: {e}] "
+
+        if i > 0:
+            segment_text = remove_overlap_duplicates(previous_segment_text, segment_text)
+
+        recognized_text += " " + segment_text
+        previous_segment_text = segment_text.split()
+
+        os.remove(segment_path)
 
     return recognized_text
+
+
+# Функция для удаления дубликатов на стыке фрагментов (перекрытие) с учетом частичного совпадения
+def remove_overlap_duplicates(previous_segment_text: list, current_segment_text: str) -> str:
+    overlap_count = 4
+
+    previous_segment_text = previous_segment_text[-overlap_count:]
+    previous_segment_text_lower = [word.lower() for word in previous_segment_text]
+
+    current_segment_words = current_segment_text.split()
+    current_segment_text = current_segment_words[:overlap_count]
+    current_segment_text_lower = [word.lower() for word in current_segment_text]
+
+    common_words = set(previous_segment_text_lower) & set(current_segment_text_lower)
+    print(common_words)
+
+    filtered_first_words = [
+        word for word in current_segment_text if word.lower() not in common_words
+    ]
+
+    remaining_words = current_segment_words[overlap_count:]
+
+    return " ".join(filtered_first_words + remaining_words)
+
+
+# Функция для объединения всех строк в одну
+def merge_lines(text: str) -> str:
+    return ' '.join(text.splitlines())
 
 
 # Функция для очистки текста от знаков препинания, приведения к нижнему регистру, замены ё на е и удаления лишних пробелов
@@ -54,24 +115,15 @@ def clean_text(text: str) -> str:
     return text
 
 
-# Функция для объединения всех строк в одну
-def merge_lines(text: str) -> str:
-    return ' '.join(text.splitlines())
-
-
 # Функция для вычисления схожести слов с учетом Levenshtein Distance
 def are_words_similar(word1: str, word2: str) -> bool:
-    # Проверяем замену окончаний "ие" на "ье" и наоборот
     if (word1.endswith("ие") and word2.endswith("ье") and word1[:-2] == word2[:-2]) or \
             (word1.endswith("ье") and word2.endswith("ие") and word1[:-2] == word2[:-2]):
         return True
-
-    # Проверяем замену окончаний "ия" на "ье" и наоборот для слов "наслаждения" и "наслажденье"
     if (word1.endswith("ия") and word2.endswith("ье") and word1[:-2] == word2[:-2]) or \
             (word1.endswith("ье") and word2.endswith("ия") and word1[:-2] == word2[:-2]):
         return True
 
-    # Проверяем расстояние Левенштейна для остальных случаев
     distance = Levenshtein.distance(word1, word2)
     if distance <= 1:
         return True
@@ -100,60 +152,5 @@ def jaccard_similarity_with_fuzzy(recognized_text: str, original_text: str) -> f
 
     union = len(recognized_words) + len(original_words)
 
-    # Вычисление процента совпадения
     similarity = (intersection / union) * 100
     return round(similarity, 2)
-
-
-# Разбиение аудио на 40-секундные фрагменты с перекрытием и удалением дубликатов на стыке фрагментов
-def split_audio(input_path: str, segment_length_ms: int = 39000, overlap_ms: int = 2000, min_segment_length_ms: int = 2000):
-    audio = AudioSegment.from_wav(input_path)
-    duration_ms = len(audio)
-
-    if duration_ms > segment_length_ms:
-        print(f"Длительность аудио больше 39 секунд ({duration_ms / 1000} секунд). Разбиваем на фрагменты.")
-        segments = []
-        for i in range(0, duration_ms, segment_length_ms - overlap_ms):
-            segment = audio[i:i + segment_length_ms]
-
-            if len(segment) < min_segment_length_ms:
-                continue
-
-            segment_path = f"audio/segment_{i // (segment_length_ms - overlap_ms)}.wav"
-            segment.export(segment_path, format="wav")
-            segments.append(segment_path)
-
-        return segments
-    else:
-        print(f"Длительность аудио меньше или равна 39 секундам ({duration_ms / 1000} секунд).")
-        return [input_path]
-
-
-# Функция для удаления дубликатов на стыке фрагментов (перекрытие)
-def remove_overlap_duplicates(segments: list) -> str:
-    merged_text = ""
-    seen_words = set()
-    previous_segment_text = []
-
-    for i, segment_path in enumerate(segments):
-        with open(segment_path, 'r') as segment:
-            words = segment.read().split()
-
-        if i > 0:
-            current_segment_text = words[:2]
-            previous_segment_text = previous_segment_text[-2:]
-
-            # Проверка на дублирование первых 2 слов с предыдущим сегментом
-            if current_segment_text == previous_segment_text:
-                words = words[2:]
-
-        filtered_words = [word for word in words if word not in seen_words]
-
-        merged_text += " ".join(filtered_words) + " "
-        seen_words.update(filtered_words)
-
-        previous_segment_text = words
-
-        os.remove(segment_path)
-
-    return merged_text.strip()
